b6c00def037befc3f5bffa47a0d4d336ac7d8e84
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2023-03-19 18:43:07 +0700

    add: model test case

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from api.models import Task
+
+
+class TaskTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.task = Task.objects.create(
+            title="This task for test", description="foo bar test test 1234", completed=False
+        )
+
+    def test_task_content(self):
+        self.assertEqual(self.task.title, "This task for test")
+        self.assertEqual(self.task.description, "foo bar test test 1234")
+        self.assertFalse(self.task.completed)
