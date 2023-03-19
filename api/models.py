88fbefeeb5531b55b05cf62f07402cdf31539f28
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2023-03-19 09:01:57 +0700

    add: task model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=80)
+    description = models.CharField(max_length=150)
+    date_created = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField()
+
+    class Meta:
+        ordering = ["-date_created"]
+
+    def __str__(self):
+        return self.title
